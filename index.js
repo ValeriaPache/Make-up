1d238953ba45be1ebd1747eaf3a29888064fb17f
Valeria Pacheco <vpachecosanmartin@gmail.com>
2024-10-27 20:06:39 -0500

    Organize la estructura y cambie el nombre de las clases a ingles

--- index.js
+++ index.js
@@ -1,88 +1,88 @@
-let carrito = [];
-let cuerpo = document.querySelector(".bolsa table tbody");
-let precio = 0;
-let botones = document.querySelectorAll(".productos .producto button");
-let cuenta = document.querySelector(".bolsa .cuenta");
-let total = document.getElementById("precio5");
+let cart = [];
+let body = document.querySelector(".bag table tbody");
+let price = 0;
+let buttons = document.querySelectorAll(".subcontainerProducts .product button");
+let account = document.querySelector(".bag .account");
+let total = document.getElementById("price5");
 
-function anadir() {
+function add() {
     let name = this.parentNode.querySelector("h3").textContent;
-    let price = parseFloat(this.parentNode.querySelector("p").textContent); // Conversion directa a número
-    let url = this.parentNode.querySelector(".imagen img").getAttribute("src");
+    let price1 = parseFloat(this.parentNode.querySelector("p").textContent); // Conversion directa a número
+    let url = this.parentNode.querySelector(".image img").getAttribute("src");
 
     // Añade el producto al carrito
-    carrito.push({ name, price, url });
-    localStorage.setItem("articulos", JSON.stringify(carrito));
-    cuenta.textContent = carrito.length;
+    cart.push({ name, price1, url });
+    localStorage.setItem("articles", JSON.stringify(cart));
+    account.textContent = cart.length;
 
-    // Suma al precio total
-    precio += price;
-    total.textContent = precio.toFixed(2); // Muestra el total en el formato adecuado
-    localStorage.setItem("precio", precio.toFixed(2));
+    // Suma al price total
+    price += price1;
+    total.textContent = price.toFixed(2); // Muestra el total en el formato adecuado
+    localStorage.setItem("price", price.toFixed(2));
 
-    actualizar();
+    update();
 }
 
-function actualizar() {
-    cuerpo.innerHTML = "";
+function update() {
+    body.innerHTML = "";
 
-    carrito.forEach((item, index) => {
+    cart.forEach((item, index) => {
         let fila = document.createElement("tr");
 
-        let imagen = document.createElement("img");
-        imagen.src = item.url;
-        let imagenTd = document.createElement("td");
-        imagenTd.appendChild(imagen);
-        fila.appendChild(imagenTd);
+        let image = document.createElement("img");
+        image.src = item.url;
+        let imageTd = document.createElement("td");
+        imageTd.appendChild(image);
+        fila.appendChild(imageTd);
 
         fila.innerHTML += `
             <td>${item.name}</td>
-            <td>${item.price.toFixed(2)}</td>
-            <td><a href="#" onclick="eliminar(${index});">X</a></td>
+            <td>${item.price1.toFixed(2)}</td>
+            <td><a href="#" onclick="eliminate(${index});">X</a></td>
         `;
 
-        cuerpo.appendChild(fila);
+        body.appendChild(fila);
     });
 }
 
-function eliminar(index) {
-    let item = carrito[index];
-    carrito.splice(index, 1);
+function eliminate(index) {
+    let item = cart[index];
+    cart.splice(index, 1);
 
-    precio -= item.price; // Resta el precio eliminado del total
-    total.textContent = precio.toFixed(2);
-    cuenta.textContent = carrito.length;
+    price -= item.price1; // Resta el price eliminado del total
+    total.textContent = price.toFixed(2);
+    account.textContent = cart.length;
 
-    localStorage.setItem("articulos", JSON.stringify(carrito));
-    localStorage.setItem("precio", precio.toFixed(2));
-    actualizar();
+    localStorage.setItem("articles", JSON.stringify(cart));
+    localStorage.setItem("price", price.toFixed(2));
+    update();
 }
 
-window.addEventListener("load", cargar);
+window.addEventListener("load", loadi);
 
-function cargar() {
-    let itemlocal = localStorage.getItem("articulos");
-    let preciolocal = localStorage.getItem("precio");
+function loadi() {
+    let itemlocal = localStorage.getItem("articles");
+    let pricelocal = localStorage.getItem("price");
 
     if (itemlocal) {
-        carrito = JSON.parse(itemlocal);
-        precio = preciolocal ? parseFloat(preciolocal) : 0;
-        total.textContent = precio.toFixed(2);
-        cuenta.textContent = carrito.length;
-        actualizar();
+        cart = JSON.parse(itemlocal);
+        price = pricelocal ? parseFloat(pricelocal) : 0;
+        total.textContent = price.toFixed(2);
+        account.textContent = cart.length;
+        update();
     }
 }
 
-function vaciar2() {
-    cuerpo.innerHTML = "";
-    cuenta.textContent = "0";
-    precio = 0;
-    carrito = [];
-    localStorage.removeItem("articulos");
-    localStorage.removeItem("precio");
+function empty2() {
+    body.innerHTML = "";
+    account.textContent = "0";
+    price = 0;
+    cart = [];
+    localStorage.removeItem("articles");
+    localStorage.removeItem("price");
     total.textContent = "0.00";
 }
 
-botones.forEach(btn => {
-    btn.addEventListener("click", anadir);
+buttons.forEach(btn => {
+    btn.addEventListener("click", add);
 });
